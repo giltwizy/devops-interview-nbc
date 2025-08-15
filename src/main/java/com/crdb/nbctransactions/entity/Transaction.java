e7a502d145f2e1fec57f74a88a1e702e89a39d44
Gilton Teti <gilton.teti@crdbbank.co.tz>
2025-08-15 13:17:03 +0300

    add validation dependency

--- src/main/java/com/crdb/nbctransactions/entity/Transaction.java
+++ src/main/java/com/crdb/nbctransactions/entity/Transaction.java
@@ -4,6 +4,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 
 @Entity
 public class Transaction {
@@ -12,7 +14,16 @@ public class Transaction {
     private Long id;
     public String service;
     public String name;
+
+    @NotNull(message = "Amount can not be NULL")
+    @NotEmpty(message = "Amount can not be Empty")
     public String amount;
+
+    @NotNull(message = "Account can not be NULL")
+    @NotEmpty(message = "Account can not be Empty")
     public String account;
+
+    @NotNull(message = "reference can not be NULL")
+    @NotEmpty(message = "reference can not be Empty")
     public String reference;
 }
